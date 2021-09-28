73517706fc01832c493279eb8dc09eac06359f6e
Jan Nils Ferner <contact@jnferner.com>
2021-09-28 16:24:46 +0200

    Add rounds

--- programming_for_biology/complex_loops/rolling_dice.py
+++ programming_for_biology/complex_loops/rolling_dice.py
@@ -10,8 +10,24 @@ def roll_dice(n):
     return rd.randint(1, 7, n)
 
 
+def count_sixes(results):
+    """
+    Count the number of sixes in a list of integers.
+    """
+    return sum(1 for roll in results if roll == 6)
+
+
+def simulate_dice_game(rolls_per_round, rounds):
+    """
+    Simulate rolling dice and counting sixes.
+    """
+    return [count_sixes(roll_dice(rolls_per_round)) for _ in range(rounds)]
+
+
 if __name__ == "__main__":
     rd.seed(141)
-    results = roll_dice(30)
-    sixes = [i for i in results if i == 6]
-    print(f"Rolled {len(sixes)} sixes")
+
+    rolls_per_round = 30
+    rounds = 10
+    results = simulate_dice_game(rolls_per_round, rounds)
+    print(results)
